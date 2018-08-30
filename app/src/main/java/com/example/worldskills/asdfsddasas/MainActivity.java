effe726a8b2a2e346ff1edae249ed798a65b259c
Sebastian <bgjuan8@misena.edu.co>
2018-08-30 10:44:16 -0500

    Nada

--- app/src/main/java/com/example/worldskills/asdfsddasas/MainActivity.java
+++ app/src/main/java/com/example/worldskills/asdfsddasas/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.worldskills.asdfsddasas;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 public class MainActivity extends AppCompatActivity {
     int tres;
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AlertDialog.Builder mensaje = new AlertDialog.Builder(MainActivity.this);
+        LayoutInflater inflater = getLayoutInflater();
+
+        //mensaje.setView(adaptador);
+        mensaje.create().show();
     }
 }
